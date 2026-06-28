b71add40b9e95a62703e2d579d738ab7c49c5cf1
tejasvigowrabathini-creator <tejasvigowrabathini@gmail.com>
2026-06-28 20:20:05 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
@@ -5,6 +6,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Analytics />
     <App />
   </StrictMode>,
 );
